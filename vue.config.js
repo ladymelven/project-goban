8fbb312d53443f8407f9dbd3e00fa31f6eb320ea
ladymelven <ladymelven@yandex.ru>
2020-10-30 01:17:32 +0300

    update config to run on django plugin-less

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,13 @@
-const BundleTracker = require('webpack-bundle-tracker');
-
 module.exports = {
-  outputDir: './dist/',
-  filenameHashing: true,
+  outputDir: '../goban/static/goban/dist',
+  indexPath: '../../templates/base-vue.html',
+
   chainWebpack: config => {
-    config.optimization
-      .splitChunks(false);
-    config
-      .plugin('BundleTracker')
-      .use(BundleTracker, [{ filename: '../project-goban/webpack-stats.json' }]);
-    config.resolve.alias
-      .set('__STATIC__', 'static');
     config.devServer
       .public('http://127.0.0.1:8080')
-      .host('127.0.0.1')
-      .port(8080)
       .hotOnly(true)
-      .watchOptions({ poll: 1000 })
-      .https(false)
-      .headers({ 'Access-Control-Allow-Origin': ['\*'] });
+      .headers({ 'Access-Control-Allow-Origin': '*' })
+      .writeToDisk(filePath => filePath.endsWith('index.html'));
   },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/project-goban/'
